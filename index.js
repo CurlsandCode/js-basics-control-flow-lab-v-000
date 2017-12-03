4080fa07c89c3682a56631d292999b46d8540429
Monika Williams <monika.m.williams@gmail.com>
2017-12-03 10:19:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,31 @@
 function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
     return "This one is on me!";
-} else if ( feet > 2000 && feet < 2500) {
-  return "I will gladly take your thirty bucks.";
-} else {
+  } else if ( feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
   return "No can do.";
+  }
 }
-}
-function ternaryCheckCity (city){
+
+function ternaryCheckCity (city) {
   if (city === 'NYC') {
-  return 'Ok, sounds good.';
-} else {
-return  "No go.";
+   return 'Ok, sounds good.';
+  } else {
+    return  "No go.";
+ }
 }
+
+function switchOnCharmFromTip (tip) {
+ switch (tip) {
+   case 'generous':
+     return "Thank you so much."
+     break;
+     case 'not so generous':
+       return "Thank you."
+       break;
+   default:
+      return 'Bye.'
+     
+ } 
 }
